e20b1a459ed467d11aaabe5ea5a91d4c0f7387d7
Dielias32 <dielias32@hotmail.com>
2026-01-06 21:36:01 -0300

    att codloop for

--- Exercicios/src/Loops/LoopFor.java
+++ Exercicios/src/Loops/LoopFor.java
@@ -4,6 +4,34 @@ public class LoopFor {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
+		
+		/*
+		 * **Usa-se mais for quando já se sabe a quantidade de vezes necessarias para o loop**
+		 * Variavel de inicializacao == i, j, k, etc...
+		 * Condicao -> determina a'te quando ou quantas vezes, o loop vai rodar
+		 * Incremento -> a variavel vai chegar na condicao
+		 */
+		
+		// conta de 1 a 5
+		for(int i = 1; i <=5; i++) {
+			System.out.println("Contador " + i);
+		}
+		
+		//contador de palavras
+		String palavra = "Java";
+		
+		for(int i = 0; i < palavra.length(); i++) {
+			
+			System.out.println(palavra + " tem " + palavra.charAt(i) + " caracteres!");
+		}
+		
+		
+		//Contagem regreciva
+		for(int i = 5; i > 0; i--) {
+			System.out.println("Contador" + i);
+		}
 
 	}
 
